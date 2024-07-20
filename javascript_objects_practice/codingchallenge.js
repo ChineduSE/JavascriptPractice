80d314fc1cfc95f82ffc484beac47269b1224cb6
ChineduSE <chinedunweke618@gmail.com>
2024-07-20 14:26:49 -0700

    coding challenge

--- javascript_objects_practice/codingchallenge.js
+++ javascript_objects_practice/codingchallenge.js
@@ -1,1 +1,26 @@
 // CODING CHALLENGE
+
+const Mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.6,
+    calcBMI: function(){
+        this.bmi = this.mass/(this.height * this.height);
+        return this.bmi;
+    }
+}
+
+const John = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function(){
+        this.bmi = this.mass/(this.height * this.height);
+        return this.bmi;
+    }
+}
+
+// console.log(`${John.calcBMI()}`);
+// console.log(`${John.bmi}`);
+// console.log(`${John.fullName}'s BMI (${John.calcBMI()}`) 
+console.log(`${John.fullName}'s BMI (${John.calcBMI()}) is ${John.calcBMI() > Mark.calcBMI() ? 'higher' : 'lower'} than ${Mark.fullName}'s BMI (${Mark.calcBMI()})!`);
