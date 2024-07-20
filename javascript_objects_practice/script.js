6bbbf1bceada799d5860ebc3d2f4bc71a2ae865e
ChineduSE <chinedunweke618@gmail.com>
2024-07-19 20:27:04 -0700

    more objects

--- javascript_objects_practice/script.js
+++ javascript_objects_practice/script.js
@@ -11,7 +11,22 @@ const ChineduInfo = {
 
   futureAge: function(){
     return this.age + 5;
-  }
+  },
+
+  // if statement within function method in an object
+  licenseSts: function(){
+    if (this.driversLicense == true)
+      this.licenseStsValue = 'a';
+    else this.licenseStsValue = 'no';
+
+    return this.licenseStsValue;
+  },
+  // easier method
+  getSummary: function(){
+    return  `${ChineduInfo.first_name}, ${this.driversLicense
+      ? 'has' : "doesn't have"} a driver's license.`
+    }
+
 };
 
 // assigning more properties to Chinedu's Info
@@ -20,6 +35,7 @@ ChineduInfo.wife = 'Holy Ghost';
 ChineduInfo.juniorSister1 = 'Chidebere';
 ChineduInfo.juniorSister2 = 'Grace';
 ChineduInfo.brothers = ['Chinonso', 'Uchenna'];
+ChineduInfo.driversLicense = true;
 
 
 console.log(`Chinedu's age is ${ChineduInfo.age}`);
@@ -32,6 +48,7 @@ if (ChineduInfo[UserResp]) console.log(UserFeedback);
 else console.log("Chinedu doesn't have such info!");
 
 
+
 // console.log(ChineduInfo);
 // console.log(ChineduInfo.brothers[1]);
 
@@ -41,4 +58,8 @@ const finalcomment = (`${ChineduInfo.first_name} has ${ChineduInfo.friends.lengt
 console.log(`${ChineduInfo.first_name}'s current age is 
 ${ChineduInfo.age}. This is the year ${CurrentYear}.
 In the next 5 years, Chinedu will be ${ChineduInfo.futureAge()}
-years old`);
+years old. Currently he has ${ChineduInfo.licenseSts()} driver's license`);
+
+ console.log(ChineduInfo.licenseStsValue);
+
+console.log(ChineduInfo.getSummary());
