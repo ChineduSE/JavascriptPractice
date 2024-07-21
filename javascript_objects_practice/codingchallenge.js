0d4474d051397c0e0b0c9ff0482a2d5d739a14ab
ChineduSE <chinedunweke618@gmail.com>
2024-07-21 04:01:49 -0700

    Javascript Update

--- javascript_objects_practice/codingchallenge.js
+++ javascript_objects_practice/codingchallenge.js
@@ -1,26 +1,56 @@
 // CODING CHALLENGE
 
-const Mark = {
+// const Mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.6,
+//     calcBMI: function(){
+//         this.bmi = this.mass/(this.height * this.height);
+//         return this.bmi;
+//     }
+// }
+// const John = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function(){
+//         this.bmi = this.mass/(this.height * this.height);
+//         return this.bmi;
+//     }
+// }
+
+// console.log(`${John.fullName}'s BMI (${John.calcBMI()}) is ${John.calcBMI() > Mark.calcBMI() ? 'higher' : 'lower'} than ${Mark.fullName}'s BMI (${Mark.calcBMI()})!`);
+
+// Instructor's Answer
+
+const mark = {
     fullName: 'Mark Miller',
     mass: 78,
     height: 1.6,
     calcBMI: function(){
-        this.bmi = this.mass/(this.height * this.height);
+        this.bmi = this.mass/this.height ** 2;
         return this.bmi;
     }
 }
-
-const John = {
+const john = {
     fullName: 'John Smith',
     mass: 92,
     height: 1.95,
     calcBMI: function(){
-        this.bmi = this.mass/(this.height * this.height);
+        this.bmi = this.mass/this.height ** 2;
         return this.bmi;
     }
 }
 
-// console.log(`${John.calcBMI()}`);
-// console.log(`${John.bmi}`);
-// console.log(`${John.fullName}'s BMI (${John.calcBMI()}`) 
-console.log(`${John.fullName}'s BMI (${John.calcBMI()}) is ${John.calcBMI() > Mark.calcBMI() ? 'higher' : 'lower'} than ${Mark.fullName}'s BMI (${Mark.calcBMI()})!`);
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if(mark.bmi > john.bmi){
+    console.log(`${mark.fullName}'s BMI (${mark.bmi})
+    is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi){
+    console.log(`${john.fullName}'s BMI (${john.bmi})
+    is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
---
+++ loops_practices/script.js
@@ -0,0 +1,74 @@
+// for loop
+
+for(let rep = 1; rep <= 10; rep++){
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+//Foor Loop arrays, breaking and Continuing
+
+const chinedu = [
+  'Chinedu',
+  'Nweke',
+  2024 - 1999,
+  'Software Engineer',
+  ['Jesuovie', 'Peculiar', 'Samuel'],
+  'Holy Ghost',
+  true
+];
+
+// const types =[];
+
+// // arrays are zero based when reading values from an array
+// for(let i = 0; i < chinedu.length; i++) {
+//   // Reading from Chinedu array
+//   console.log(chinedu[i], typeof chinedu[i]);
+//   // filling types arrays
+//   // types[i] = typeof chinedu[i];
+//   types.push(typeof chinedu[i]); //writes to the end of the array.
+// }
+
+// console.log (types);
+
+// // Calculating ages and storing into an array
+
+const currentYear = 2024;
+const birthyear = [1999, 2000, 2002, 2001, 1997];
+const age = [];
+
+for(let i = 0; i < birthyear.length; i++){
+  const calcAge = function(){
+    return currentYear - birthyear[i];
+  }
+  age.push(calcAge());
+  console.log (`Age no ${birthyear[i]} is ${age[i]}`);
+}
+console.log(age);
+
+//instructor's Answer
+// const years = [1999, 2000, 2002, 2001, 1997];
+// const ages = [];
+
+// for(let i = 0; i < years.length; i++){
+//   ages.push(2024 - years[i]);
+// }
+// console.log(ages);
+
+
+// Continue and break statement
+// Continue helps to exit from the current iteration and
+// move to another iteration
+
+console.log('---ONLY STRINGS---')
+for(let i = 0; i < chinedu.length; i++) {
+  if(typeof chinedu[i] !== 'string') continue; 
+  console.log(chinedu[i], typeof chinedu[i]);
+}
+// break exits the entire loop not just the iteration
+
+console.log('---STOP WHEN YOU FIND NUMBER---')
+for(let i = 0; i < chinedu.length; i++) {
+  if(typeof chinedu[i] == 'number') break; 
+  console.log(chinedu[i], typeof chinedu[i]);
+}
+
+//looping arrays backwards 
